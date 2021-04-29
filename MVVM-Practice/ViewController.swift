b9ec2055a7ff8c92b6419eb8e34fa1e205476c3e
Gam <gam@appsynth.net>
2021-04-29 12:03:15 +0700

    Initial Commit

---
+++ MVVM-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Practice
+//
+//  Created by Gam Doung on 29/4/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
